1fd46c706207d689556d8d0515bca0510ca34212
Scott Ankiewicz <scottfa.a@gmail.com>
2023-01-25 15:02:20 -0500

    set up authentication middleware

--- config/env.js
+++ config/env.js
@@ -2,7 +2,10 @@ const EXT_PORT = 8000
 
 const DATABASE_URL = 'mongodb://localhost/game-tracker-dev'
 
+const JWT_SECRET = process.env.JWT_SECRET || 'development-secret'
+
 module.exports = {
   EXT_PORT,
   DATABASE_URL,
+  JWT_SECRET
 }
