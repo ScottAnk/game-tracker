434668792976d48ae0459d0900e9e73b43bc8d6a
Scott Ankiewicz <scottfa.a@gmail.com>
2023-01-26 22:35:32 -0500

    request logging and CORS permission

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const ENV = require('./config/env')
 const express = require('express')
+const cors = require('cors')
+
+const ENV = require('./config/env')
 const mongoose = require('./db/connection')
+const requestLogger = require('./lib/requestLogger')
 
 const app = express()
 
+app.use(cors({origin: 'http://127.0.0.1:5500'}))
 app.use(express.json())
+app.use(requestLogger)
 
 const userRoutes = require('./routes/userRoutes')
 const gameRoutes = require('./routes/gameRoutes')
