af89419440dbecd9af01b96e9af12184b46c2306
Scott Ankiewicz <scottfa.a@gmail.com>
2023-01-26 22:43:16 -0500

    implement db seed() drop() reset()

--- config/env.js
+++ config/env.js
@@ -3,9 +3,11 @@ const EXT_PORT = 8000
 const DATABASE_URL = 'mongodb://localhost/game-tracker-dev'
 
 const JWT_SECRET = process.env.JWT_SECRET || 'development-secret'
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'lol great password'
 
 module.exports = {
   EXT_PORT,
   DATABASE_URL,
   JWT_SECRET,
+  ADMIN_PASSWORD,
 }
