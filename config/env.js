7f99e3f74fc26f10cbc143195b9af987e1c43423
Scott Ankiewicz <scottfa.a@gmail.com>
2023-01-31 22:46:42 -0500

    fix: track environment module

---
+++ config/env.js
@@ -0,0 +1,15 @@
+const EXT_PORT = 8000
+
+const DATABASE_URL = 'mongodb://localhost/game-tracker-dev'
+
+const JWT_SECRET = process.env.JWT_SECRET || 'development-secret'
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'lol great password'
+const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://127.0.0.1:5500'
+
+module.exports = {
+  EXT_PORT,
+  DATABASE_URL,
+  JWT_SECRET,
+  ADMIN_PASSWORD,
+  CORS_ORIGIN,
+}
